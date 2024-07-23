902eaea69456e9ba96a54cd22f8dacfc9dde92bc
Maksim Sukhov <silveraven@yandex.ru>
2024-07-23 17:13:04 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let result = "";
+    let str = Math.abs(n).toString();
+    let i = str.length - 1;
+
+  while (i >= 0) {
+    result += str[i];
+    i -= 1
+  }
+  return parseInt(result);
 }
